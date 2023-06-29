abcd7e281f2d18abc7acf0f2a6c0af38f26c5ac2
Sergey <Sergey@localhost.localdomain>
2023-06-29 10:58:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import json
+import time
+from os.path import exists
+from urllib.parse import urljoin
+
+import requests
+from environs import Env
+
+
+def fetch_ep_credentials(api_base_url, client_id, client_secret):
+    url = urljoin(api_base_url, 'oauth/access_token')
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, data=data)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print(response.json(), '\n', response.text)
+    return response.json()
+
+
+def get_access_token(credentials_path, api_base_url, client_id, client_secret):
+    if exists(credentials_path):
+        with open(credentials_path, 'r') as json_file:
+            credentials = json.loads(json_file.read())
+
+        current_time = time.time()
+        expiration_time = credentials.get('expires')
+        if current_time < expiration_time:
+            return credentials.get('access_token')
+
+    credentials = fetch_ep_credentials(api_base_url, client_id, client_secret)
+    with open(credentials_path, 'w') as json_file:
+        json_file.write(json.dumps(credentials))
+    return credentials.get('access_token')
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    api_base_url = env('EP_API_URL')
+    client_id = env('EP_CLIENT_ID')
+    client_secret = env('EP_CLIENT_SECRET')
+
+
+
+    moltin_access_token = get_access_token('credentials.json', api_base_url, client_id, client_secret)
+    print(moltin_access_token)
+
+    headers = {'Authorization': f'Bearer {moltin_access_token}'}
+    url = url = urljoin(api_base_url, 'pcm/products')
+    #response = requests.get(url, headers=headers)
+    #response.raise_for_status()
+
+    #print(response.json())
